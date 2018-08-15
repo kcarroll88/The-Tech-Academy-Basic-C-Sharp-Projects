3b22b3718d3a845153069b0e3fc6ddcbb36a0598
Keenan Carroll <keenan.carroll.va@gmail.com>
2018-08-15 15:32:15 -0400

    ACME accounting test

---
+++ ConsoleApp1/ACME_accounting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class ACME_accounting
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to ACME Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
